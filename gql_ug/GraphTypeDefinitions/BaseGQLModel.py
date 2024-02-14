633e9a095b8df000c364c7715b295408987b129f
MinhQuanTo <115225096+MinhQuanTo@users.noreply.github.com>
2024-02-15 00:39:53 +0100

    Update BaseGQLModel.py

--- gql_ug/GraphTypeDefinitions/BaseGQLModel.py
+++ gql_ug/GraphTypeDefinitions/BaseGQLModel.py
@@ -3,14 +3,17 @@ import uuid
 import datetime
 import typing
 
+# Definuje typ ID jako GraphQL ID, ale pak jej přepisuje na typ UUID
 IDType = strawberry.ID
 IDType = uuid.UUID
 
 class BaseGQLModel:
+    # Metoda určená k přepsání v odvozených třídách, získává loader pro načítání dat.
     @classmethod
     def getLoader(cls, info):
         pass
-
+        
+    # Asynchronně načítá a řeší referenci entity na základě jejího ID.
     @classmethod
     async def resolve_reference(cls, info: strawberry.types.Info, id: IDType):
         if id is None:
@@ -19,7 +22,7 @@ class BaseGQLModel:
         if isinstance(id, str): id = uuid.UUID(id)
         result = await loader.load(id)
         if result is not None:
-            result.__strawberry_definition__ = cls.__strawberry_definition__  # little hack :)
+            result.__strawberry_definition__ = cls.__strawberry_definition__ 
         return result
 
 
