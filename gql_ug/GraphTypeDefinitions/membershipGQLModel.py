43feb1428612976b8e7aac62c1214a237baf2c72
MinhQuanTo <115225096+MinhQuanTo@users.noreply.github.com>
2024-02-15 01:38:35 +0100

    Update membershipGQLModel.py

--- gql_ug/GraphTypeDefinitions/membershipGQLModel.py
+++ gql_ug/GraphTypeDefinitions/membershipGQLModel.py
@@ -5,6 +5,7 @@ from typing import List, Optional, Union, Annotated
 import gql_ug.GraphTypeDefinitions
 from .BaseGQLModel import BaseGQLModel, IDType
 
+# Imports resolver functions for various fields of the GraphQL model.
 from ._GraphResolvers import (
     resolve_id,
     resolve_name,
@@ -15,47 +16,57 @@ from ._GraphResolvers import (
     resolve_createdby
 )
 
+# Function to retrieve the loader from the GraphQL context, used for fetching data.
 def getLoader(info):
     return info.context["all"]
 
+# Annotations for lazy loading of GraphQL models to prevent circular dependencies.
 GroupGQLModel = Annotated["GroupGQLModel", strawberry.lazy(".groupGQLModel")]
 UserGQLModel = Annotated["UserGQLModel", strawberry.lazy(".userGQLModel")]
 
+# Defines a GraphQL model for a Membership entity, including its fields and resolvers.
 @strawberry.federation.type(
     keys=["id"],
     description="""Entity representing a relation between an user and a group""",
 )
 class MembershipGQLModel(BaseGQLModel):
+    # Class method to get the loader specific for memberships.
     @classmethod
     def getLoader(cls, info):
         return getLoader(info).memberships
 
+    # Field resolvers for the Membership model.
     id = resolve_id
     changedby = resolve_changedby
     created = resolve_created
     lastchange = resolve_lastchange
     createdby = resolve_createdby
 
+    # Async resolver for the user field, returning a UserGQLModel or None.
     @strawberry.field(description="""user""")
     async def user(self, info: strawberry.types.Info) -> Optional["UserGQLModel"]:
         # return self.user
         result = await gql_ug.GraphTypeDefinitions.UserGQLModel.resolve_reference(info=info, id=self.user_id)
         return result
 
+    # Async resolver for the group field, returning a GroupGQLModel or None.
     @strawberry.field(description="""group""")
     async def group(self, info: strawberry.types.Info) -> Optional["GroupGQLModel"]:
         # return self.group
         result = await gql_ug.GraphTypeDefinitions.GroupGQLModel.resolve_reference(info=info, id=self.group_id)
         return result
 
+    # Field for checking if the membership is still valid.
     @strawberry.field(description="""is the membership is still valid""")
     async def valid(self) -> Union[bool, None]:
         return self.valid
 
+    # Field for the start date of the membership.
     @strawberry.field(description="""date when the membership begins""")
     async def startdate(self) -> Union[datetime.datetime, None]:
         return self.startdate
 
+    # Field for the end date of the membership.
     @strawberry.field(description="""date when the membership ends""")
     async def enddate(self) -> Union[datetime.datetime, None]:
         return self.enddate
@@ -65,10 +76,13 @@ class MembershipGQLModel(BaseGQLModel):
 # Special fields for query
 #
 #####################################################################
+# Special fields for query filtering.
 from .utils import createInputs
 from dataclasses import dataclass
 GroupInputWhereFilter = Annotated["GroupInputWhereFilter", strawberry.lazy(".groupGQLModel")]
 UserInputWhereFilter = Annotated["UserInputWhereFilter", strawberry.lazy(".userGQLModel")]
+
+# Dataclass for filtering memberships in queries.
 @createInputs
 @dataclass
 class MembershipInputWhereFilter:
@@ -85,6 +99,7 @@ class MembershipInputWhereFilter:
 #####################################################################
 import datetime
 
+# Mutation input model for updating a membership.
 @strawberry.input
 class MembershipUpdateGQLModel:
     id: IDType
@@ -93,6 +108,7 @@ class MembershipUpdateGQLModel:
     startdate: Optional[datetime.datetime] = None
     enddate: Optional[datetime.datetime] = None
 
+# Mutation input model for inserting a new membership.
 @strawberry.input
 class MembershipInsertGQLModel:
     user_id: IDType
@@ -102,16 +118,19 @@ class MembershipInsertGQLModel:
     startdate: Optional[datetime.datetime] = None
     enddate: Optional[datetime.datetime] = None
 
+# GraphQL model for the result of a membership operation.
 @strawberry.type
 class MembershipResultGQLModel:
     id: IDType = None
     msg: str = None
 
+    # Resolver for fetching the membership operation result.
     @strawberry.field(description="""Result of membership operation""")
     async def membership(self, info: strawberry.types.Info) -> Union[MembershipGQLModel, None]:
         result = await MembershipGQLModel.resolve_reference(info, self.id)
         return result
-    
+
+# Mutation for updating a membership.
 @strawberry.mutation(description="""Update the membership, cannot update group / user""")
 async def membership_update(self, 
     info: strawberry.types.Info, 
@@ -130,7 +149,7 @@ async def membership_update(self,
     
     return result
 
-
+# Mutation for inserting a new membership.
 @strawberry.mutation(description="""Inserts new membership""")
 async def membership_insert(self, 
     info: strawberry.types.Info, 
